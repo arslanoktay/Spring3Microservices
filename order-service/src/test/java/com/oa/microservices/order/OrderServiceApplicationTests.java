0162f90d315a386aba3892ea0e22badf10fc4526
Oktayy16 <oktay16arslan@hotmail.com>
2025-01-07 21:37:16 +0300

    order service tamamlandı

--- order-service/src/test/java/com/oa/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/oa/microservices/order/OrderServiceApplicationTests.java
@@ -1,15 +1,58 @@
 package com.oa.microservices.order;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MySQLContainer;
+import static org.hamcrest.MatcherAssert.assertThat;
+import org.hamcrest.Matchers;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+// Integration Test
+//@Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class OrderServiceApplicationTests {
 
+	@ServiceConnection
+	static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.3.0");
+	@LocalServerPort
+	private Integer port;
+
+	static { // static ctor, java da birden fazla blok açılabilir ve bunlar sırası ile çalışacaktır!!!
+		mySQLContainer.start();
+	}
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
+	}
+
 	@Test
-	void contextLoads() {
+	void shouldSubmitOrder() {
+		String submitJson = """
+				{
+				    "skuCode": "iPhone_15",
+				    "price": 1000,
+				    "quantity": 1
+				}
+				""";
+		var responseBodyString = RestAssured.given()
+				.contentType("application/json")
+				.body(submitJson)
+				.when()
+				.post("/api/order")
+				.then()
+				.log().all()
+				.statusCode(201)
+				.extract()
+				.body()
+				.asString();
+
+		assertThat(responseBodyString,Matchers.is("Order Placed Successfully"));
 	}
 
 }
